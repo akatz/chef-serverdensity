babf1bd6a9017efff7b1b444d3477d1fcd0b3997
Avrohom Katz <iambpentameter@gmail.com>
2012-11-20 16:26:26 -0500

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: serverdensity
+# Attributes:: default
+#
+# Copyright 2012, Escape Studios
+#
+
+default['serverdensity']['repository_key'] = "boxedice-public.key"
+default['serverdensity']['agent_key'] = "CHANGE_ME"
+default['serverdensity']['sd_url'] = "CHANGE_ME"
